df8d747e790019aeee0459079bdd5c2285467a7c
khadija <khadijalahmar@gmail.com>
2021-07-16 15:27:35 +0100

    creation_megration_produit_and_MOdel_controllerProduit

---
+++ app/Models/produit.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Routing\Route;
+
+class produit extends Model
+{
+    use HasFactory;
+
+}
+
---
+++ database/migrations/2021_07_14_134844_create_produit.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduit extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produit', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name_product');
+            $table->string('descreption');
+            $table->integer('prix');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produit');
+    }
+}
