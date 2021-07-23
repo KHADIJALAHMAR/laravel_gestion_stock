f3fbbffcf1b543b56b83f2df0587842381969b5d
khadija <khadijalahmar@gmail.com>
2021-07-23 21:14:22 +0100

    create_view_update

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js%22%3E"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js%22%3E"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js%22%3E"></script>
     <title>Create produit</title>
 </head>
 
@@ -15,22 +19,35 @@
     <form action="{{ route('store') }}" method="post">
         {{-- {{ route('blog.store') }} --}}
         @csrf
-
-        <input type="text" name="name_product" id="name_product" value="{{ old('name_product') }}" placeholder="name_product">
-        @error('name_product')
-            {{ $message }}
-        @enderror
-
+        <div class="mb-3">
+            <input type="text" class="form-control" name="name_product" id="name_product" value="{{ old('name_product') }}" placeholder="name_product">
+            <div class="text-danger">
+                @error('name_product')
+                {{ $message }}
+            @enderror
+            </div>
+           
+          </div>
         <br />
-
-        <input type="text" name="descreption"  value="{{ old('descreption') }}" id="descreption" placeholder="descreption">
-        @error('descreption')
+        <div class="mb-3">
+            <input type="text" name="descreption"   class="form-control" value="{{ old('descreption') }}" id="descreption" placeholder="descreption">
+            <div class="text-danger">
+                @error('descreption')
+                {{ $message }}
+                @enderror
+            </div>   
+        </div>
+        <div class="mb-3">
+         <input type="text" name="prix" class="form-control" value="{{ old('prix') }}" id="prix" placeholder="prix">
+        <div class="text-danger">
+            @error('prix')
             {{ $message }}
-        @enderror
-        <input type="text" name="prix"  value="{{ old('prix') }}" id="prix" placeholder="prix">
-        @error('prix')
-        {{ $message }}
-    @enderror
+            @enderror
+        </div>     
+        
+       
+      
+    
 
         <input type="submit" value="save">
 
